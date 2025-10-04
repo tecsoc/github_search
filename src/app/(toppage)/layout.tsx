8b023d86eb941cc23f39dea0c5ae8a927b29d6e5
t-tecsoc <25559838+t-tecsoc@users.noreply.github.com>
2025-10-04 09:14:46 +0900

    chore: スタイルを整える

--- src/app/(toppage)/layout.tsx
+++ src/app/(toppage)/layout.tsx
@@ -1,14 +1,16 @@
+import styles from './layout.module.scss';
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <>
-      <header>
+    <div className={styles.body_wrapper}>
+      <header className={styles.header}>
         <h1>GitHub リポジトリ検索サイト</h1>
       </header>
       {children}
-    </> 
+    </div>
   );
 }
