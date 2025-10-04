39b55baaa2f078fa6434ac6a39d277b0d15916f3
t-tecsoc <25559838+t-tecsoc@users.noreply.github.com>
2025-10-04 11:54:12 +0900

    feat: 無限ローディングの実装

--- src/app/(toppage)/page.tsx
+++ src/app/(toppage)/page.tsx
@@ -4,18 +4,24 @@ import { gql } from "@apollo/client";
 import { useLazyQuery } from "@apollo/client/react";
 import { Button, TextInput } from "@mantine/core";
 import { useCallback, useRef } from "react";
+import InfiniteScroll from "react-infinite-scroller";
 import RepositoryItem from "./areas/RepositoryItem";
 import styles from "./page.module.scss";
 
+const ITEMS_PER_PAGE = 10;
+
 const SEARCH_REPOSITORIES = gql`
-  query SearchRepositories($query: String!, $first: Int!) {
-    search(query: $query, type: REPOSITORY, first: $first) {
+  query SearchRepositories($query: String!, $first: Int!, $after: String) {
+    search(query: $query, type: REPOSITORY, first: $first, after: $after) {
       repositoryCount
+      pageInfo {
+        endCursor
+        hasNextPage
+      }
       edges {
         node {
           ... on Repository {
             name
-            nameWithOwner
             description
             url
             stargazerCount
@@ -24,6 +30,7 @@ const SEARCH_REPOSITORIES = gql`
               name
             }
             owner {
+              login
               avatarUrl(size: 100)
             }
           }
@@ -35,12 +42,37 @@ const SEARCH_REPOSITORIES = gql`
 export default function Home() {
 
   const queryRef = useRef<HTMLInputElement>(null);
-  const [searchRepos, { data, loading, error }] = useLazyQuery(SEARCH_REPOSITORIES);
+  const [searchRepos, { data, loading, fetchMore }] = useLazyQuery(SEARCH_REPOSITORIES);
   const handleSearchButton = useCallback(() => {
     if (!queryRef.current?.value) return;
     console.log({data})
     searchRepos({ variables: { query: queryRef.current?.value, first: 10 } });
-  },[queryRef]);
+  },[data, searchRepos]);
+
+  const loadMore = useCallback( () => {
+    if (!queryRef.current?.value) return;
+    if (!data?.search.pageInfo.hasNextPage) return;
+    fetchMore({
+      variables: {
+        query: queryRef.current?.value,
+        first: 10,
+        after: data?.search?.pageInfo.endCursor,
+      },
+      updateQuery: (previousResult, { fetchMoreResult }) => {
+        if (!fetchMoreResult) return previousResult;
+
+        return {
+          search: {
+            ...fetchMoreResult.search,
+            edges: [
+              ...previousResult.search.edges,
+              ...fetchMoreResult.search.edges,
+            ],
+          },
+        };
+      },
+    });
+  },[data?.search.pageInfo.endCursor, data?.search.pageInfo.hasNextPage, fetchMore]);
 
   return (
     <main className={styles.main}>
@@ -48,17 +80,24 @@ export default function Home() {
         <TextInput ref={queryRef} className={styles.text_input} placeholder="リポジトリ名" required />
         <Button variant={"light"} size="md" onClick={handleSearchButton}>検索</Button>
       </div>
-      <div className={styles.repositories}>
-        {data?.search?.edges.map(node => {
+      <InfiniteScroll
+        className={styles.repositories}
+        pageStart={0}
+        loadMore={loadMore}
+        hasMore={!!data?.search.pageInfo.hasNextPage}
+        loader={<div key={0}>Loading ...</div>}
+      >
+        {data?.search?.edges.map((node, index: string) => {
           const item = node["node"]
-          const name = item["name"];
+          const name = item["name"]
+          const owner = item["owner"]
           return <RepositoryItem
-            key={name}
+            key={index}
             name={name}
-            avatarUrl={item["owner"]["avatarUrl"]}
+            avatarUrl={owner["avatarUrl"]}
           />;
         })}
-      </div>
+      </InfiniteScroll>
     </main>
   );
 }
